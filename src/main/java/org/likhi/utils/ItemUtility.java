161feafccb614b43640143919a76398989bca9b9
Likhil45 <likhilcharan.vellanki99@gmail.com>
2022-09-28 21:10:44 -0500

    Change in Menu

--- src/main/java/org/likhi/utils/ItemUtility.java
+++ src/main/java/org/likhi/utils/ItemUtility.java
@@ -11,7 +11,8 @@ public class ItemUtility {
 	private List<MyItem> topics= Arrays.asList(
 			new MyItem("1", "Dosa", " Dosa Batter"),
 			new MyItem("2", "Idli", " dli batter"),
-			new MyItem("3", "Pavbaji", "Bun with Aloo curry"));
+			new MyItem("3", "Pavbaji", "Bun with Aloo curry")
+			new MyItem("4", "Upma", "Ravva"));
 	
 	public List<MyItem> getAlltopics(){
 		return topics;
