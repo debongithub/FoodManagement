6def213e9325a43e2fef21359225f562d88b7cef
Likhil45 <likhilcharan.vellanki99@gmail.com>
2022-09-28 20:56:27 -0500

    Change-Id: I0000000000000000000000000000000000000000
    Change-Id: Ia43a179f4b36581f2270700e7f6eebbac9603489

--- src/main/java/org/likhi/controller/HelloController.java
+++ src/main/java/org/likhi/controller/HelloController.java
@@ -2,7 +2,7 @@ package org.likhi.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-//Hi
+
 @RestController
 public class HelloController {
 	@RequestMapping("/hi{HI}")
