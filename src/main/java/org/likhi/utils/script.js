8e6a8dd87257c3678496daac0cce2053a86d0f3d
Likhil45 <likhilcharan.vellanki99@gmail.com>
2022-09-30 16:31:33 -0500

    Script added

---
+++ src/main/java/org/likhi/utils/script.js
@@ -0,0 +1,13 @@
+var settings = {
+  "url": "google.com",
+  "method": "GET",
+  "timeout": 0,
+  "headers": {
+    "Authorization": "Basic YWJjOnh5eg==",
+    "Cookie": "NID=511=ZUKFFa-ccZR04C5om6MQWJinrgsyJHQ3m4oZ5Zfou2Iyo9RZS_FujsuEQxLfLJwkBjwr5DI1PDHhmdTpVIGuPNUvvOrVDhk8vBiJiDJvXaaSn_37TKBpQxnZ-XNtKgvjXBcTJNWZ9IwuLi2lyhBn8CYe1mLuAxvgi9Qb6zNmYks"
+  },
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
